59e75a581fa611e87fe9209de0aa60da5f735264
David Jacobs <djacobs@gmail.com>
2011-01-15 18:06:31 -0500

    perltidy'd generate.pl, and cleaned up some unused variables & libraries, and reverted "too smart" hours shifting

--- generate.pl
+++ generate.pl
@@ -2,68 +2,69 @@
 
 use strict;
 
-use constant SERVER => 'AIdev';
-use constant DEBUG => 1;
+use constant SERVER     => 'AIdev';
+use constant DEBUG      => 0;
 use constant DEBUG_SAVE => 0;
 
 use constant USER => 'xxxx';
 use constant PASS => 'xxxx';
 
 # libraries
-use Data::Dumper;
-use File::Glob qw(:globally :nocase);
+#use Data::Dumper;
+#use File::Glob qw(:globally :nocase);
 use JSON;
 use MongoDB;
 
-# globals or reused
-my ($key, $value);
-my $now = time;
-my (%setid, $setid);
-my ($ii, $tt);
-my %datakey;
-my @datavar;
+# globals or reused variables
+my ( %setid,      $setid );
+my ( $ii,         $tt );
+my ( %datakey,    @datavar );
+my ( $connection, $database, $collection );
 
-my $connection = MongoDB::Connection->new( host => 'dbh00.mongolab.com', port => 27007 );
+$connection =
+  MongoDB::Connection->new( host => 'dbh00.mongolab.com', port => 27007 );
 $connection->authenticate( 'smddj1', USER, PASS );
-my $database = $connection->smddj1;
-my $collection = $database->aidev2;
-
+$database   = $connection->smddj1;
+$collection = $database->aidev2;
 
 sub debug {
-   DEBUG and print "DEBUG: @_\n";
-   DEBUG_SAVE and print STDERR @_,"\n";
+    DEBUG and print "DEBUG: @_\n";
+    DEBUG_SAVE and print STDERR @_, "\n";
 }
 
 # more info is available here: http://search.cpan.org/dist/MongoDB/lib/MongoDB/Collection.pm
 # or here http://www.mongodb.org/display/DOCS/Advanced+Queries
-# TODO - it appears that we can't .group with MongoDB.pm. It's OK. 
-my $data       = $collection->find( { }, {'setid' => 1 } );
+# TODO - it appears that we can't .group with MongoDB.pm. That's OK.
+my $data = $collection->find( {}, { 'setid' => 1 } );
 
-while (my $ii = $data->next) {
-   # debug($ii->{'setid'});
-   $setid{$ii->{'setid'}} = 1;
+while ( my $ii = $data->next ) {
+
+    # debug($ii->{'setid'});
+    $setid{ $ii->{'setid'} } = 1;
 }
 
-foreach (keys %setid) {
-   my $data = $collection->find( { 'setid' => $_ });
-   $setid = $_;
-   while ($ii = $data->next) {
-      # debug(Dumper($ii));
-      # my $hour = (localtime($ii->{'time'}))[2], "\n";
-      $datakey{$setid}{'name'} = $setid;
-      $datakey{$setid}{'hour'} = ((localtime($ii->{'time'}))[2] - 3) % 24;
-      $datakey{$setid}{$ii->{'operation'}} = $ii->{'value'};
-   }   
-   # push hash into array
-   push (@datavar, $datakey{$setid});
+foreach ( keys %setid ) {
+    my $data = $collection->find( { 'setid' => $_ } );
+    $setid = $_;
+    while ( $ii = $data->next ) {
+
+        # debug(Dumper($ii));
+        # my $hour = (localtime($ii->{'time'}))[2], "\n";
+        $datakey{$setid}{'name'} = $setid;
+        $datakey{$setid}{'hour'} = ( localtime( $ii->{'time'} ) )[2];
+        $datakey{$setid}{ $ii->{'operation'} } = $ii->{'value'};
+    }
+
+    # push hash into array
+    push( @datavar, $datakey{$setid} );
 }
 
 # TODO: don't hard code this location
 my $write_file = '/Users/djacobs/Desktop/protovis/scratch/stats2.js';
-open my ($fh), '>', $write_file or die 'could not open stats file for writing';
-print $fh 'var stats = '. encode_json(\@datavar) .';';
-close $fh;
 
+open my ($fh), '>', $write_file or die 'could not open stats file for writing';
+print $fh 'var stats = ' . encode_json( \@datavar ) . ';';
+close $fh;
 
 __END__ 
 
